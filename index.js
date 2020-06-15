0429f78cd0f33823237a6d471a6e0a976bae2b37
Beekasha <brian.j.kleiner@gmail.com>
2020-06-15 19:31:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+// Challenge 1
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+// Challenge 2
+const random = Math.random()
+
+
+// Challenge 3
+const mod = 16 % 6;
+
+
+// Challenge 4
+const max = Math.max(1,2,3,20);
