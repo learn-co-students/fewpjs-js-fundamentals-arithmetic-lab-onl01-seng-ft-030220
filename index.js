50d9f710bc129fbb5da0824cc427fd469a47e05b
Craig Christopher Messi <craig.messi@gmail.com>
2020-06-22 19:58:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = 31 * 2
+
+const random = Math.random() + 1
+
+const mod = 25 % 7
+
+const max = Math.max(1, 4, 8, 16, 20)
